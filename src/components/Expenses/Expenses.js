4cca5bf84f8a3d89f1d2747bf9d217e3a3a99426
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-06 17:06:58 -0300

    55.Lec Multiple states

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,11 +4,13 @@ import './ExpenseItem.css'
 import './Expenses.css'
 import ExpenseDate from './ExpenseDate'
 import ExpenseItem from './ExpenseItem'
+import NewExpense from './../NewExpense/NewExpense'
 import Card from '../UI/Card'
 
 export default function Expenses(props){
     return (
         <>
+        <NewExpense></NewExpense>
         <div className='expenses'>
             {props.listOfItems.map((e)=>{
                 return (
