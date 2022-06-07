b093cf9ba0b6b7ce240151b4aa2d21e8605c20a9
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-07 13:57:22 -0300

    assignment 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import './ExpenseDate.css'
-import './ExpenseItem.css'
-import './Expenses.css'
-import ExpenseDate from './ExpenseDate'
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card'
+import React from "react";
+import "./ExpenseDate.css";
+import "./ExpenseItem.css";
+import "./Expenses.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import Card from "../UI/Card";
 
-export default function Expenses(props){
-    return (
-        <>
-        <div className='expenses'>
-            {props.listOfItems.map((e)=>{
-                return (
-                    <Card>
-                        <ExpenseItem
-                            title={e.title}
-                            amount={e.amount}
-                            date={e.date}
-                        />
-                    </Card>
-                ) 
-            })}
-        </div>
-        </>
-    )
+export default function Expenses(props) {
+
+  function getDataFromFilter(data) {
+    console.log(".");
+    console.log("data from expensesFilter");
+    console.log(data);
+    console.log("data from expensesFilter");
+    console.log(".");
+  }
+
+  return (
+    <>
+      <ExpensesFilter liftingFilterData={getDataFromFilter} />
+      <div className="expenses">
+        {props.listOfItems.map((e) => {
+          return (
+            <Card>
+              <ExpenseItem title={e.title} amount={e.amount} date={e.date} />
+            </Card>
+          );
+        })}
+      </div>
+    </>
+  );
 }
